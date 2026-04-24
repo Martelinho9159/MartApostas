5c98407f03d29e90dd5e5012a638395a73cb4888
Martelinho9159 <gamerluciano07@gmail.com>
2026-04-24 13:41:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,9 +8,22 @@ let money = Number(localStorage.getItem("money")) || 100
 let h1Rise = document.getElementById("h1Rise")
 let inputRise = document.getElementById("RiseMoneyInput")
 let inputButton = document.getElementById("RiseMoneyButton")
+let inround = false
+let alreadyaposta = false
+const interval = 5
+const task = {}
+const originalcolor = h1Rise.style.color
 
-function wait(seconds) {
+task.wait = function(seconds) {
   return new Promise(resolve => setTimeout(resolve, seconds * 1000))
+}
+
+async function StartNextRound() {
+while (await task.wait(interval)) {
+
+
+
+}
 }
 
 function random(MaxParam) {
@@ -59,17 +72,33 @@ function GetMoneyOnInput() {
   return Number(inputRise.value)
 }
 
+StartNextRound()
+
 async function StartGame() {
+  if (!inround) {
+    inround = true
+  if (h1Rise.style.color === "red") {
+    h1Rise.style.color = originalcolor
+  }
+
   for (let i = 0; i < 99999999; i++) {
-    h1Rise.innerText = Math.floor((1 + i / 100) * 100) / 100 + "x"
+    let apostado = GetMoneyOnInput()
+    let mont = Math.floor((1 + i / 100) * 100) / 100
+    h1Rise.innerText = mont + "x" + " " +   "(+" + Math.floor((apostado * mont) * 100) / 100 + "R$)"
     let stop = random(75)
     if (stop === 1) {
       h1Rise.innerText = "Crashed"
       h1Rise.style.color = "red"
+      inround = false
       break
     }
-    await wait(0.2)
+
+    await task.wait(0.2)
+
   }
+} else {
+  alert("Espere até que o próximo round aconteça!")
+}
 }
 
 async function StartAposta() {
